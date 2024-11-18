e9b2da9b427cf9a0cd7816c7df8e8a4da240e84e
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-11-18 14:01:27 +0500

    push

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CLIENT_ID } from '../../hooks/useEnv'
 
 function Login() {
-	const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_IDn}&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-recently-played`
+	const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-recently-played`
 
 	return (
 		<div className="w-full h-[100vh] flex items-center justify-center bg-login">
